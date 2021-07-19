5a4f19363ddfd37da82366c4898bd388902ea0d0
Evelin Aasna <evelin.aasna@fu-berlin.de>
2021-07-19 19:03:28 +0200

    [MISC] Basic project using raptor as example

---
+++ src/hashmap_build.cpp
@@ -0,0 +1,8 @@
+#include <build.hpp>
+
+void hashmap_build(build_arguments const & arguments)
+{
+    run_program(arguments);
+
+    return;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <sstream>
+#include <argument_parsing.hpp>
 
 #include <seqan3/argument_parser/all.hpp>
 #include <seqan3/core/debug_stream.hpp>
@@ -7,27 +7,24 @@
 
 int main(int argc, char ** argv)
 {
-    seqan3::argument_parser myparser{"Low-memory-prefilter", argc, argv};        // initialise myparser
-    cmd_arguments args{};
- 
-    initialise_argument_parser(myparser, args);
-
-    try
+   try
     {
-         parser.parse();                                                  // trigger command line parsing
+        seqan3::argument_parser top_level_parser{"hashmap", argc, argv, seqan3::update_notifications::on, {"build", "search"}};
+        
+	hashmap::init_top_level_parser(top_level_parser);
+        hashmap::try_parsing(top_level_parser);
+
+        seqan3::argument_parser & sub_parser = top_level_parser.get_sub_parser();
+        if (sub_parser.info.app_name == std::string_view{"hashmap-build"})
+            hashmap::run_build(sub_parser, false);
+        if (sub_parser.info.app_name == std::string_view{"hashmap-search"})
+            hashmap::run_search(sub_parser, false);
     }
-    catch (seqan3::argument_parser_error const & ext)                     // catch user errors
+    catch (seqan3::argument_parser_error const & ext)
     {
-        seqan3::debug_stream << "Parsing error. " << ext.what() << "\n"; // give error message
-        return -1;
+        std::cerr << "[Parsing error] " << ext.what() << '\n';
+        std::exit(-1);
     }
-
-    // convert_fastq(fastq_file, output_file); // Call fastq to fasta converter
-
-    if (verbose) // if flag is set
-        seqan3::debug_stream << "Conversion was a success. Congrats!\n";
-
-
 
     return 0;
 }
